7ff641fb7edc862e86ad97d898444d2ca4d0dc9b
jrferrera <jrferrera17@gmail.com>
2015-10-12 08:16:11 +0800

    Added initial session files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  match 'logout' => 'sessions#destroy', via: [:all]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
